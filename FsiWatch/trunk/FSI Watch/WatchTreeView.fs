cac414b6be4edba686305e55f357d94de47c9b34
sswen4sa <sswen4sa@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-04-21 07:22:09 +0000

    added fields to the mix: not too sure about my abstraction, but i think it may just be winforms in general is not the greatest

--- FsiWatch/trunk/FSI Watch/WatchTreeView.fs
+++ FsiWatch/trunk/FSI Watch/WatchTreeView.fs
@@ -2,44 +2,86 @@
 open System.Windows.Forms
 open System.Reflection
 
+type ModelKind = 
+    | Field
+    | Property
+//    | Enumerable
+//    | Type
+
+type ModelProtection =
+    | Private
+    | Public
+
+type Model = { Name:string; LazyValue: Lazy<obj>; Kind: ModelKind; Protection: ModelProtection; Type: System.Type }
+
 type WatchTreeView() as this =
     inherit TreeView()
 
-    let updateNode (tn:TreeNode) name tag =
+    //should also include type here
+    let getRootNodeText name tag = sprintf "%s: %s" name (if obj.ReferenceEquals(tag, null)  then "null" else tag.ToString())
+
+    let updateNode (tn:TreeNode) name tag text =
         tn.Tag <- tag
         tn.Name <- name
-        tn.Text <- sprintf "%s: %s" name (if obj.ReferenceEquals(tag, null)  then "null" else tag.ToString())
+        tn.Text <- text
 
-    let createNode name tag = 
+    let createNode name tag text = 
         let tn = new TreeNode()
-        updateNode tn name tag
+        updateNode tn name tag text
         tn
 
     let updateNodeChildren (tn:TreeNode) =
         tn.Nodes.Clear()
         if tn.Tag <> null then
-            let props = 
-                tn.Tag.GetType().GetProperties(BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Public)
-                |> Seq.sortBy (fun p -> p.Name.ToLower())
+            let propertyModels = seq {
+                let props = 
+                    tn.Tag.GetType().GetProperties(BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Public)
 
-            for p in props do
-                //printfn "%A updating child with %A" tn.Tag p.Name
-                
-                //this is a hack, this.AccessiblityObject.Parent hangs, probably due to a lock
-                //need to implement timeout, perhaps configurable
-                //no longer needed once we moved watch tree view out
-                //let skip = (obj.ReferenceEquals(tn.Tag, this.AccessibilityObject)) && p.Name = "Parent"
-                // && (not skip)
-                if p.GetIndexParameters() = Array.empty then //non-indexed property
-                    try
-                        //printfn "before getvalue"
-                        let propValue = p.GetValue(tn.Tag, Array.empty)
-                        //printfn "after getvalue"
+                for p in props do
+                    if p.GetIndexParameters() = Array.empty then //non-indexed property
                         let isPublic  = p.GetGetMethod(true).IsPublic
-                        ignore <| tn.Nodes.Add(createNode (sprintf "%s (%s property)" p.Name (if isPublic then "public" else "private")) propValue)
-                    with e ->
-                        //printfn "%A" e
-                        ignore <| tn.Nodes.Add(createNode p.Name e)
+                        let propValue = lazy (
+                            try
+                                p.GetValue(tn.Tag, Array.empty)
+                            with e ->
+                                e :> obj
+                        )
+                        yield {Name = p.Name; LazyValue = propValue; Kind = Property; Protection = (if isPublic then Public else Private); Type = p.PropertyType}
+            }
+
+            let fieldModels = seq {
+                let fields = 
+                    tn.Tag.GetType().GetFields(BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Public)
+
+                for f in fields do
+                    let isPublic  = f.IsPublic
+                    let fieldValue = lazy (
+                        try 
+                            f.GetValue(tn.Tag)
+                        with e ->
+                            e :> obj
+                    )
+                    yield {Name = f.Name; LazyValue = fieldValue; Kind = Field; Protection = (if isPublic then Public else Private); Type = f.FieldType}
+            }
+
+            let models = seq {yield! propertyModels; yield! fieldModels}
+            let modelNodes =
+                models
+                |> Seq.map
+                    (fun m ->
+                        createNode 
+                            m.Name  
+                            m.LazyValue.Value
+                            (sprintf "%s (%s %s %s): %s" 
+                                (m.Name)
+                                (m.Protection |> function | Public -> "public" | Private -> "private") 
+                                (m.Kind |> function | Field -> "field" | Property -> "property")
+                                (m.Type.Name)
+                                (if obj.ReferenceEquals(m.LazyValue.Value, null) then "null" else m.LazyValue.Value.ToString())))
+                |> Seq.sortBy (fun n -> n.Name.ToLower())
+                |> Seq.toArray
+
+            tn.Nodes.AddRange(modelNodes)
     do
         //when expanding a node, add all immediate children to each child
         this.AfterExpand.Add (fun args -> for node in args.Node.Nodes do updateNodeChildren node)
@@ -47,7 +89,7 @@ type WatchTreeView() as this =
         member this.UpdateWatch(tn:TreeNode, tag) =
             this.BeginUpdate()
             (
-                updateNode tn tn.Name tag
+                updateNode tn tn.Name tag (getRootNodeText tn.Name tag)
                 updateNodeChildren tn
             )
             this.EndUpdate()
@@ -56,7 +98,7 @@ type WatchTreeView() as this =
             this.BeginUpdate()
             (
                 //create new node and add all it's immediate children
-                let node = createNode name tag
+                let node = createNode name tag (getRootNodeText name tag)
                 updateNodeChildren node
                 ignore <| this.Nodes.Add(node)
             )
