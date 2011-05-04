835c16bf2c99b4c07ca212d8351f4a8782dd9c30
sswen4sa <sswen4sa@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-05-04 00:30:13 +0000

    Fix startup script

--- FsiWatch/branches/async/FSI Watch/Startup.fsx
+++ FsiWatch/branches/async/FSI Watch/Startup.fsx
@@ -1,12 +1,13 @@
-﻿#r @"C:\Documents and Settings\swensens\My Documents\Visual Studio 2010\Projects\Unquote\Unquote\bin\Release\Unquote.dll"
+﻿#r @"C:\Unquote\Unquote.dll"
 open Swensen.Unquote
 #load "WatchTreeModel.fs"
 #load "WatchTreeView.fs"
 open Swensen.Watch.Forms
 #load "WatchForm.fs"
 open Swensen.Watch.Forms
-#load "InteractiveSessionExt.fs"
+#load "FsiHelper.fs"
 open Swensen.Watch.Fsi
+open Microsoft.FSharp.Compiler.Interactive
 
 
 //----testing todo, need to refactor out to model view
@@ -18,38 +19,68 @@ open Swensen.Watch.Fsi
 
 
 //add refresh all
+
+open Swensen.Watch.Forms
+
+//initialize watch and attached fsi listener
+let watch = new WatchForm()
+
+fsi.AddPrintTransformer <|
+    //need to figure out a way to not call repeatedly for single evaluation
+    fun (_:obj) ->
+        try
+            if watch.Visible |> not then
+                watch.Show()
+                watch.Activate()
+
+            FsiHelper.getWatchableFsiVariables()
+            |> watch.Watch
+
+            null
+        with e ->
+            printfn "%A" (e.InnerException)
+            null
+
+//only way can get at
+module FsiWatch = 
+    let watch = watch
     
 
 //Simple example of how we can "break" during evaluation!
 async {
     for i in 1..40 do
-        fsi.watch.Watch("i", i)
-        fsi.watch.Watch("i*2", i*2)
-        fsi.watch.Archive()
+        watch.Watch("i", i)
+        watch.Watch("i*2", i*2)
+        watch.Archive()
         if i % 10 = 0 then
             System.Threading.Thread.Sleep(50)
-            do! fsi.watch.AsyncBreak()
+            do! watch.AsyncBreak()
 } |> Async.StartImmediate
 
-fsi.watch.AsyncContinue()
-fsi.watch.Show()
+watch.AsyncContinue()
+watch.Show()
 
+
+let work loops = 
+    for i in 1I..(loops*1000000I) do () 
+    loops
+    
 type SlowType() =
-    member this.One = System.Threading.Thread.Sleep(1000) ; 1
-    member this.Two = System.Threading.Thread.Sleep(2000) ; 2
-    member this.Three = System.Threading.Thread.Sleep(3000) ; failwith "Some exception occurred" ; 3
-    member this.Four = System.Threading.Thread.Sleep(4000) ; 4
-    member this.Four3 = System.Threading.Thread.Sleep(6000) ; 4
-    member this.Four2 = System.Threading.Thread.Sleep(7000) ; 4
-    member this.Four1 = System.Threading.Thread.Sleep(8000) ; 4
-    member this.Fou5 = System.Threading.Thread.Sleep(9000) ; 4
-    member private this.Fou23 = System.Threading.Thread.Sleep(1000) ; 4
-    member this.Fou234 = System.Threading.Thread.Sleep(12000) ; 4
-    member this.Fousd = System.Threading.Thread.Sleep(32000) ; 4
-    member private this.Fous = System.Threading.Thread.Sleep(4000) ; 4
-    member this.Foug = System.Threading.Thread.Sleep(4000) ; 4
+    member this.One = work 1I
+    member this.Two = work 1I
+    member this.Three = work 1I |> ignore ; failwith "Some exception occurred" ; 3
+    member this.Four = work 1I
+    member this.Four3 = work 1I
+    member this.Four2 = work 2I
+    member this.Four1 = work 4I
+    member this.Fou5 = work 3I
+    member private this.Fou23 = work 5I
+    member this.Fou234 = work 6I
+    member this.Fousd = work 1I
+    member private this.Fous =work 3I
+    member this.Foug = work 2I
 
-fsi.watch.Watch("test!", 23)
+watch.Watch("test!", 23)
 
 //Async.Parallel(
 
