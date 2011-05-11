e58ead81d236a5a9760c47c90b4a2a1fee13daa7
stephen.swensen <stephen.swensen@gmail.com>
2011-05-11 03:58:52 +0000

    Added SeqExt

--- FsEye/MiscUtils.fs
+++ FsEye/MiscUtils.fs
@@ -7,3 +7,11 @@ let inline (=&) a b = obj.ReferenceEquals(a,b)
 ///Not reference equals
 let inline (<>&) a b = a =& b |> not
 
+
+            
+let xl = [None, 1; Some 1, 2; Some 0, 2;]
+
+xl
+|> distinctByResolve
+    snd
+    (fun x y -> compare (fst x) (fst y))
