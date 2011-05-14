5f92644ec6296539c3e1613c46deaaa58278e88d
stephen.swensen@gmail.com <stephen.swensen@gmail.com@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-05-14 14:26:30 +0000

    initial work including methods as part of the model

--- branches/methods/FsEye/WatchTreeModel.fs
+++ branches/methods/FsEye/WatchTreeModel.fs
@@ -22,7 +22,7 @@ open Swensen.Utils
 
 //how to add icons to tree view: http://msdn.microsoft.com/en-us/library/aa983725(v=vs.71).aspx
 
-let private sprintValue (value:obj) (ty:Type) =
+let private sprintValue (value:obj) (ty:Type) = //BUG: NEED TO MAKE SURE TY IS NOT NULL
     let cleanString (str:string) = str.Replace("\n","").Replace("\r","").Replace("\t","")
 
     match value with
@@ -39,7 +39,8 @@ and MemberInfo = { LoadingText:string ; AsyncInfo: Lazy<string * seq<Watch>>}
 and CustomInfo = { Text: string ; Children:seq<Watch>}
 and Watch =
     | Root of RootInfo
-    | Member of  MemberInfo
+    | DataMember of  MemberInfo
+    | CallMember of  MemberInfo
     | Custom of CustomInfo
     with 
         member this.RootInfo =
@@ -49,41 +50,76 @@ and Watch =
         member this.DefaultText =
             match this with
             | Root(info) -> info.Text
-            | Member(info) -> info.LoadingText
+            | DataMember(info) | CallMember(info) -> info.LoadingText
             | Custom(info) -> info.Text
         member this.Children =
             match this with
             | Root(info) -> info.Children
-            | Member(info) -> info.AsyncInfo.Value |> snd
+            | DataMember(info) | CallMember(info) -> info.AsyncInfo.Value |> snd
             | Custom(info) -> info.Children
 
 ///Create lazy seq of children s for a typical valued 
 let rec createChildren (value:obj) (ty:Type) =
-    seq {
-        yield! createType ty
-        yield! createResults value
-        yield! createMembers value
-    } //maybe |> Seq.cache
-///Type , if type info exists
-and createType ty = 
-    seq {
-        match ty with
-        | null -> ()
-        | _ -> 
-            let tyty = ty.GetType()
-            let text = sprintf "GetType() : %s = typeof<%s>" tyty.FSharpName ty.FSharpName
-            let children = createChildren ty (ty.GetType())
-            yield Custom({Text=text ; Children=children})
-    }
-///Results , if value is IEnumerable
-and createResults value =
-    seq {
-        match value with
-        | :? System.Collections.IEnumerable as value -> 
+    seq { yield! createMembers value } //maybe |> Seq.cache
+//Create a s for fields and properites, sorted by name and sub-organized by access
+and createMembers ownerValue =
+    if ownerValue =& null then Seq.empty
+    else
+        let ownerTy = ownerValue.GetType()
+        let getMembers bindingFlags =
+            let allMembers = seq {
+                ///yield all ownerTy members
+                yield! ownerTy.GetMembers(bindingFlags)
+            
+                let mapMembers tys = 
+                    tys 
+                    |> Seq.map (fun (ty:Type) -> ty.GetMembers(bindingFlags))
+                    |> Seq.concat
+                //yield all ownerTy interface members
+                yield! ownerTy.GetInterfaces() |> mapMembers
+                //yield all ownerTy base type (recursive) members
+                yield! 
+                    ownerTy 
+                    |> Seq.unfold(fun ty -> 
+                        let bty = ty.BaseType
+                        if bty = null then None else Some(bty, bty))
+                    |> mapMembers }
+
+            let validMemberTypes =
+                allMembers
+                |> Seq.filter (fun mi ->
+                    match mi with
+                    | :? PropertyInfo as pi -> pi.GetIndexParameters() = Array.empty
+                    | :? MethodInfo as meth -> 
+                        meth.GetParameters() = Array.empty && 
+                        meth.ReturnType <> typeof<System.Void> && 
+                        meth.ReturnType <> typeof<unit> &&
+                        meth.ContainsGenericParameters |> not &&
+                        meth.Name.StartsWith("get_") |> not
+                    | :? FieldInfo -> true
+                    | _ -> false)
+
+            let nonRedundantMembers =
+                validMemberTypes
+                |> Seq.distinctByResolve
+                    (fun mi -> mi.Name.ToLower())
+                    (fun mi1 mi2 -> 
+                        let ty1, ty2 = mi1.DeclaringType, mi2.DeclaringType
+                        if ty1.IsAssignableFrom(ty2) then -1
+                        elif ty2.IsAssignableFrom(ty1) then 1
+                        else 0)
+
+            let sortedMembers =
+                nonRedundantMembers
+                |> Seq.sortBy (fun mi -> mi.Name.ToLower())
+
+            sortedMembers
+
+        let createResultWatches (value:System.Collections.IEnumerator) = 
             let createChild index value =
                 //Would like to be able to always get the type
                 //but if is non-Custom IEnumerable, then can't
-                let ty = if value =& null then null else value.GetType()
+                let ty = if value =& null then typeof<obj> else value.GetType()
                 let text = sprintf "[%i] : %s = %s" index ty.FSharpName (sprintValue value ty)
                 let children = createChildren value ty
                 Custom({Text=text ; Children=children})
@@ -97,87 +133,68 @@ and createResults value =
                         yield Custom({Text="Rest" ; Children=rest})
                     else
                         yield nextResult;
-                        yield! calcRest (pos+1) ie
-            }
+                        yield! calcRest (pos+1) ie }
             
-            let children = seq {
-                yield! calcRest 0 (value.GetEnumerator()) //should use "use" when getting enumerator?
-            } // |> Seq.cache
-                
-            yield Custom({Text= sprintf "GetEnumerator() : IEnumerator" ; Children = children})
-        | _ -> ()
-    }
-//Create a s for fields and properites, sorted by name and sub-organized by access
-and createMembers ownerValue =
-    if ownerValue =& null then Seq.empty
-    else
-        let publicFlags = BindingFlags.Instance ||| BindingFlags.Public
-        let nonPublicFlags =BindingFlags.Instance ||| BindingFlags.NonPublic
+            seq { yield! calcRest 0 value } //should use "use" when getting enumerator?
 
-        //returns count * Watch
-        let props flags = 
-            let propInfos = ownerValue.GetType().GetProperties(flags)
-            propInfos.Length, 
-            seq {
-                for pi in propInfos do
-                    if pi.GetIndexParameters() = Array.empty then //non-indexed property
-                        let pretext = sprintf "(P) %s : %s = %s" pi.Name pi.PropertyType.FSharpName
+        let getPropertyWatch (pi:PropertyInfo) =
+            let pretext = sprintf "(P) %s : %s = %s" pi.Name pi.PropertyType.FSharpName
+            let delayed = lazy(
+                let value, valueTy =
+                    try
+                        pi.GetValue(ownerValue, Array.empty), pi.PropertyType
+                    with e ->
+                        box e, e.GetType()
+                if typeof<System.Collections.IEnumerator>.IsAssignableFrom(valueTy) then
+                    pretext "seq [..]", createResultWatches (value :?> System.Collections.IEnumerator)
+                else
+                    pretext (sprintValue value valueTy), createChildren value valueTy)
+            DataMember({LoadingText=(pretext "Loading...") ; AsyncInfo=delayed })
 
-                        let delayed = lazy(
-                            let value, valueTy =
-                                try
-                                    pi.GetValue(ownerValue, Array.empty), pi.PropertyType
-                                with e ->
-                                    box e, e.GetType()
+        let getFieldWatch (fi:FieldInfo) =
+            let pretext = sprintf "(F) %s : %s = %s" fi.Name fi.FieldType.FSharpName
+            let delayed = lazy(
+                let value, valueTy = 
+                    try 
+                        fi.GetValue(ownerValue), fi.FieldType
+                    with e ->
+                        box e, e.GetType()
+                if typeof<System.Collections.IEnumerator>.IsAssignableFrom(valueTy) then
+                    pretext "seq [..]", createResultWatches (value :?> System.Collections.IEnumerator)
+                else
+                    pretext (sprintValue value valueTy), createChildren value valueTy)
+            DataMember({LoadingText=(pretext "Loading...") ; AsyncInfo=delayed})
 
-                            pretext (sprintValue value valueTy), createChildren value valueTy
-                        )
-                        yield pi.Name, Member({LoadingText=(pretext "Loading...") ; AsyncInfo=delayed})
-            }
-          
-        //returns count * Watch  
-        let fields flags = 
-            let fieldInfos = ownerValue.GetType().GetFields(flags)
-            fieldInfos.Length, 
-            seq {
-                for fi in fieldInfos do
-                    let pretext = sprintf "(F) %s : %s = %s" fi.Name fi.FieldType.FSharpName
+        let getMethodWatch (mi:MethodInfo) =
+            let pretext = sprintf "(M) %s() : %s = %s" mi.Name mi.ReturnType.FSharpName
+            let delayed = lazy(
+                let value, valueTy =
+                    try
+                        mi.Invoke(ownerValue, Array.empty), mi.ReturnType
+                    with e ->
+                        box e, e.GetType()
+                if typeof<System.Collections.IEnumerator>.IsAssignableFrom(valueTy) then
+                    pretext "seq [..]", createResultWatches (value :?> System.Collections.IEnumerator)
+                else
+                    pretext (sprintValue value valueTy), createChildren value valueTy)
+            CallMember({LoadingText=(pretext "Loading...") ; AsyncInfo=delayed })
 
-                    let delayed = lazy(
-                        let value, valueTy = 
-                            try 
-                                fi.GetValue(ownerValue), fi.FieldType
-                            with e ->
-                                box e, e.GetType()
+        let getMemberWatches bindingFlags = seq {
+            let members = getMembers bindingFlags
+            for m in members do
+                match m with
+                | :? PropertyInfo as x -> yield getPropertyWatch x
+                | :? FieldInfo as x -> yield getFieldWatch x
+                | :? MethodInfo as x -> yield getMethodWatch x }
 
-                        pretext (sprintValue value valueTy), createChildren value valueTy
-                    )
-
-                    yield fi.Name, Member({LoadingText=(pretext "Loading...") ; AsyncInfo=delayed})
-            }
-
-        let getMembers flags =
-            let propCount, propSeq = props flags
-            let fieldCount, fieldSeq = fields flags
-
-            let sortedMembers =
-                Seq.append propSeq fieldSeq
-                |> Seq.sortBy (fun (name, _) -> name.ToLower())
-
-            (propCount + fieldCount), sortedMembers
-
-        let _, publicMembers = getMembers publicFlags
-        let nonPublicMembersCount, nonPublicMembers =  getMembers nonPublicFlags
+        let publicBindingFlags = BindingFlags.Instance ||| BindingFlags.Public
+        let nonPublicBindingFlags = BindingFlags.Instance ||| BindingFlags.NonPublic
 
         seq {
-            //optimization: check count instead of doing Seq.isEmpty |> not which forces
-            //full evaluation due to Seq.sortBy
-            if nonPublicMembersCount > 0 then 
-                let children = nonPublicMembers |> Seq.map snd
-                yield Custom({Text="Non-public" ; Children=children})
-            yield! publicMembers |> Seq.map snd
+            let nonPublicMemberWatches = getMemberWatches nonPublicBindingFlags
+            yield Custom({Text="Non-public" ; Children=nonPublicMemberWatches})
+            yield! getMemberWatches publicBindingFlags
         }
-
 ///Create a watch root. If value is not null, then value.GetType() is used as the watch Type instead of
 ///ty. Else if ty is not null ty is used. Else typeof<obj> is used.
 let createRootWatch (name:string) (value:obj) (ty:Type) = 
