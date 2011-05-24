80e772f19c54f2e3ed81af6f87dc93d37d34b783
stephen.swensen@gmail.com <stephen.swensen@gmail.com@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-05-24 01:11:42 +0000

    experimental refactorings and adding Copy Value context menu item

--- trunk/FsEye/Startup.fsx
+++ trunk/FsEye/Startup.fsx
@@ -41,7 +41,7 @@ let work loops =
 type SlowType() =
     member this.AMethod() = work 2I ; new SlowType()
     member this.One = work 1I
-    member this.Two = work 1I
+    member this.Two = work 2I
 //    member this.Three = work 1I |> ignore ; failwith "Some exception occurred" ; 3
 //    member this.Four = work 1I
 //    member this.Four3 = work 1I
@@ -56,58 +56,8 @@ type SlowType() =
 
 eye.Watch("sl", SlowType())
 eye.Show()
-//Async.Parallel(
-
-let f = 23;;
-
-let x = <@ "hi" @>
-
-//use the following to determine whether is generic or nongeneric or nonseq type (to display right type info)
-open System
-#r "bin/Release/Unquote.dll"
-open Swensen.Unquote
-type SeqType =
-    | Generic
-    | NonGeneric of Type
-    | NonSeq
-
-let ty = typeof<list<int>>
-let tyInterfaces = ty.GetInterfaces()
-let tyGenericSeq = tyInterfaces |> Array.tryFind(fun i -> i.IsGenericType && i.GetGenericTypeDefinition() = typedefof<seq<_>>)
-match tyGenericSeq with
-| Some(tyGenericSeq) -> tyGenericSeq.FSharpName
-| None -> "not found"
 
 //also try to avoid display immediately seq's which are lazy (e.g. known concrete type, or perhaps having Item.[int] property lookup)
-
-open System.Reflection
-let getZeroArgNonUnitMethods (ty:Type) =
-    let flags = BindingFlags.Instance ||| BindingFlags.Public ||| BindingFlags.NonPublic
-    let tyConcreteMethods = ty.GetMethods()
-    let tyInterfaces = ty.GetInterfaces() |> Seq.map (fun i -> i.GetMethods(flags)) |> Seq.concat
-    let tyMethods = Seq.append tyConcreteMethods tyInterfaces
-    tyMethods
-    |> Seq.filter 
-        (fun m -> m.ReturnType <> typeof<unit> //doesn't return unit
-                  && m.ReturnType <> typeof<Void> //doesn't return Void
-                  && m.GetGenericArguments().Length = 0 //doesn't take generic parameters
-                  && m.GetParameters().Length = 0 //doesn't take normal parameters
-                  && not ((m.Name.StartsWith("get_") || m.Name.StartsWith("set_")))) //is not a property (FSharp does not set the IsSpecialName bit: http://code.google.com/p/moq/issues/detail?id=238)
-    |> Seq.map
-        (fun m ->
-            let suffix = m.Name + "()"
-            if m.DeclaringType.IsInterface then
-                m.DeclaringType.FSharpName + "." + suffix
-            else
-                suffix)                
-    |> Seq.sortBy (fun name -> name.ToLower())
-
-    //|> Seq.map (fun m -> m.DeclaringType.FSharpName + "." + m.Name)
-
-//    tyMethods
-//    |> Seq.filter 
-//        (fun tyMethod ->
-//            tyMethod..
 
 //features
 //Monitors FSI for watch additions and updates
