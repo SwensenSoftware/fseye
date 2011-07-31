d3db19c58986779f43dbadde7d79806c0a563178
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-07-31 14:44:21 +0000

    added some missing copywrite headers

--- Utils/ControlExt.fs
+++ Utils/ControlExt.fs
@@ -1,4 +1,20 @@
-﻿namespace Swensen.Utils
+﻿(*
+Copyright 2011 Stephen Swensen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*)
+
+namespace Swensen.Utils
 
 module Control =
     let inline update< ^a when ^a :(member BeginUpdate: unit -> unit) and ^a : (member EndUpdate: unit -> unit)> (this: ^a) f =
