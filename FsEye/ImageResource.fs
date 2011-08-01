6981a85492c8bbbdda91b688508594390d349671
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-08-01 00:19:56 +0000

    refactoring

---
+++ FsEye/ImageResource.fs
@@ -0,0 +1,19 @@
+﻿namespace Swensen.FsEye
+open System
+open System.Reflection
+
+type ImageResource private (name:string) =
+    let loadImageResource =
+        let assm = Assembly.GetExecutingAssembly()
+        fun name -> System.Drawing.Image.FromStream(assm.GetManifestResourceStream(name))
+
+    let image = loadImageResource name
+
+    member __.Name = name
+    member __.Image = image
+
+    static member None = ImageResource "VSObject_Field.bmp"
+    static member Field = ImageResource "VSObject_Field.bmp"
+    static member Property = ImageResource "VSObject_Properties.bmp"
+    static member Method = ImageResource "VSObject_Method.bmp"
+
