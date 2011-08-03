3a7c66893cb4b0e939d35e4c564e4182b8ec8f6a
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-08-03 01:21:18 +0000

    improved child query functions for unit tests

--- Test.FsEye/WatchModelTests.fs
+++ Test.FsEye/WatchModelTests.fs
@@ -9,12 +9,17 @@ open Swensen.Utils
 
 ///Create a root watch, fast
 let watch x = createRootWatch "x" x null
-let findChildWithStartText startText (w:Watch) = w.Children |> Seq.find (fun x -> x.DefaultText.StartsWith startText)
-let tryFindChildWithStartText startText (w:Watch) = w.Children |> Seq.tryFind (fun x -> x.DefaultText.StartsWith startText)
+let findChildByName memberInfoName (w:Watch) = 
+    w.Children 
+    |> Seq.find (fun x -> x.MemberInfo |> function Some(mi) -> mi.Name = memberInfoName | _ -> false)
+
+let tryFindChildByName memberInfoName (w:Watch) = 
+    w.Children 
+    |> Seq.tryFind (fun x -> x.MemberInfo |> function Some(mi) -> mi.Name = memberInfoName | _ -> false)
 
 [<Fact>]
 let ``Issue 13 Fix: GetEnumerator may be called multiple times without exhaustion bug fix`` () =
-    let w = watch {1..100} |> findChildWithStartText "GetEnumerator"
+    let w = watch {1..100} |> findChildByName "GetEnumerator"
     test <@ w.Children |> Seq.length = 100 @>
     test <@ w.Children |> Seq.isEmpty |> not @>
 
