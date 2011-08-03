f9f68d2f73b892f9322900c771a879946a0868ab
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2011-08-03 01:36:33 +0000

    unit tests for sequence watches

--- trunk/Test.FsEye/WatchModelTests.fs
+++ trunk/Test.FsEye/WatchModelTests.fs
@@ -17,22 +17,41 @@ let tryFindChildByName memberInfoName (w:Watch) =
     w.Children 
     |> Seq.tryFind (fun x -> x.MemberInfo |> function Some(mi) -> mi.Name = memberInfoName | _ -> false)
 
+///Get the last element of a sequence with 1 or more elements
+let last xs = xs |> Seq.skip ((xs |> Seq.length) - 1) |> Seq.head
+
 [<Fact>]
 let ``Issue 13 Fix: GetEnumerator may be called multiple times without exhaustion bug fix`` () =
     let w = watch {1..100} |> findChildByName "GetEnumerator"
     test <@ w.Children |> Seq.length = 100 @>
     test <@ w.Children |> Seq.isEmpty |> not @>
 
-//[<Fact>]
-//let ``sequence with less than 100 elements has no Rest sub node`` () =
-//    let w = watch {1..100}
-//    let ws =
-//        w.Children
-//        |> Seq.find (fun x -> x.DefaultText.StartsWith "GetEnumerator")
-//
-//    test <@ ws.Children |> Seq.length = 100 @>
-//    test <@ ws.Children |> Seq.length = 100 @>
-//    test <@ (ws.Children |> Seq.nth 99).DefaultText <> "Rest" @>
+[<Fact>]
+let ``sequence with less than or equal to 100 elements has no Rest sub node`` () =
+    let w = watch {1..100} |> findChildByName "GetEnumerator"
 
+    test <@ w.Children |> Seq.length = 100 @>
+    test <@ (w.Children |> last).DefaultText <> "Rest" @>
 
-    
+[<Fact>]
+let ``sequence with 101 elements has Rest node in 101st position which itself contains the remaining last element node nodes`` () =
+    let w = watch {1..101} |> findChildByName "GetEnumerator"
+
+    test <@ w.Children |> Seq.length = 101 @>
+    test <@ (w.Children |> last).DefaultText = "Rest" @>
+    test <@ (w.Children |> last).Children |> Seq.length = 1 @>
+
+[<Fact>]
+let ``sequence with 110 elements has Rest node in 101st position which itself contains the remaining 10 element nodes`` () =
+    let w = watch {1..110} |> findChildByName "GetEnumerator"
+
+    test <@ w.Children |> Seq.length = 101 @>
+    test <@ (w.Children |> last).DefaultText = "Rest" @>
+    test <@ (w.Children |> last).Children |> Seq.length = 10 @>
+
+[<Fact>]
+let ``infinate sequences are handled lazily without problem`` () =
+    let w = watch (Seq.initInfinite id) |> findChildByName "GetEnumerator"
+
+    test <@ w.Children |> Seq.length = 101 @>
+    test <@ (((w.Children |> last).Children |> last).Children |> last).DefaultText = "Rest" @>
