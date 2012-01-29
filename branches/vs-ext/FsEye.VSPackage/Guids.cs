6cf7c8dfcd2f1ae1c7598c497edb44944ce1c818
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-01-29 18:52:18 +0000

    added a vspackage

---
+++ branches/vs-ext/FsEye.VSPackage/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace StephenSwensen.FsEye_VSPackage
+{
+    static class GuidList
+    {
+        public const string guidFsEye_VSPackagePkgString = "c4fcc2d5-08f8-4e73-a218-14e5fd3a684d";
+        public const string guidFsEye_VSPackageCmdSetString = "75b5d0c4-c410-4264-97e5-d2d8e1054744";
+        public const string guidToolWindowPersistanceString = "90499915-0189-4cd9-ac5c-2de5c8277b7d";
+
+        public static readonly Guid guidFsEye_VSPackageCmdSet = new Guid(guidFsEye_VSPackageCmdSetString);
+    };
+}
---
+++ branches/vs-ext/FsEye.VSPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FsEye")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stephen Swensen")]
+[assembly: AssemblyProduct("FsEye")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("FsEye.VSPackage_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009719494074d35bba9138a461a04da3f7ccf1b963942ce8b1cbaedd3bc24c7c1ecf3f6fea8544b2c25ac64a0854db19d9eb70dbce536f5b5e6b938d81d4d77e9f62ddba0b4e7b6e98b7308e2cad62c7097cd453cd5a722837053c6a0b4761b16162ff2f7160e7a973022084cad68f64b02c46eb7eaf8f7be8fae249795e0ef9ed")]
+[assembly: InternalsVisibleTo("FsEye.VSPackage_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009719494074d35bba9138a461a04da3f7ccf1b963942ce8b1cbaedd3bc24c7c1ecf3f6fea8544b2c25ac64a0854db19d9eb70dbce536f5b5e6b938d81d4d77e9f62ddba0b4e7b6e98b7308e2cad62c7097cd453cd5a722837053c6a0b4761b16162ff2f7160e7a973022084cad68f64b02c46eb7eaf8f7be8fae249795e0ef9ed")]
