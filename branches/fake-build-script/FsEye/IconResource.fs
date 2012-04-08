28ff0f7267ec205033518c4804e28b1de37cd5c6
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-04-08 17:15:53 +0000

    going to try creating a fake build script

---
+++ branches/fake-build-script/FsEye/IconResource.fs
@@ -0,0 +1,21 @@
+﻿namespace Swensen.FsEye
+open System
+open System.Reflection
+
+///Icon resource used to identify tree view node member classifications and provide FsEye form icon.
+type IconResource (name:string) = //should probably make IDisposible to dispose of Icon
+    let loadIconResource =
+        let assm = Assembly.GetExecutingAssembly()
+        fun name -> new System.Drawing.Icon(assm.GetManifestResourceStream(name))
+
+    let image = loadIconResource name
+
+    member __.Name = name
+    member __.Icon = image
+
+//F# doesn't allow public static fields so we save our selves a lot of boiler-plate using a module pretending to be the static part of the class
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>] 
+///Icon resource used to identify tree view node member classifications and provide FsEye form icon.
+module IconResource =
+    let FsEye = IconResource "FsEye.ico"
