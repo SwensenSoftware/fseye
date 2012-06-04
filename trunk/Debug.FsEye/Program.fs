e38946b6d77b0c0e53fa2d057ea5f718928edf70
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-06-04 06:38:52 +0000

    added Debug.FsEye project for running FsEye with the debugger attached.

---
+++ trunk/Debug.FsEye/Program.fs
@@ -0,0 +1,19 @@
+﻿namespace Debug.FsEye
+
+open System
+open System.Drawing
+open System.Windows.Forms
+
+module Main =
+    let initEye() =
+        let eye = new Swensen.FsEye.Forms.WatchForm()
+        eye.Watch("x", 3)        
+        eye
+
+
+    [<STAThread>]
+    [<EntryPoint>]
+    do
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(false)
+        Application.Run(initEye()) 
