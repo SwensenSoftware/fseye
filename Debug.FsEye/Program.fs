65ce298980b4762565949fda2f54d5bf426b9beb
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-07-05 04:04:12 +0000

    let debug.fseye detatch naturally

--- Debug.FsEye/Program.fs
+++ Debug.FsEye/Program.fs
@@ -7,6 +7,7 @@ open System.Windows.Forms
 module Main =
     let initEye() =
         let eye = new Swensen.FsEye.Forms.WatchForm()
+        eye.Closing.Add(fun x -> x.Cancel <- false) //undo the close supression that the watch form otherwise tries to undertake.
         eye.Watch("x", 3)  
         eye.Watch("y", new System.Collections.Generic.List<int>(Seq.init 200 id))      
         eye
