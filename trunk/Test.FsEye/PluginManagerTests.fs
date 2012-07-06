312de38af813345f92a83d88d55068b8f2d3a9bf
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-07-06 03:26:38 +0000

    starting down a path of testing plugin manager

---
+++ trunk/Test.FsEye/PluginManagerTests.fs
@@ -0,0 +1,32 @@
+﻿module PluginManagerTests
+
+open Swensen.Unquote
+open Xunit
+
+open Swensen.FsEye.WatchModel
+open Swensen.FsEye.Forms
+open Swensen.FsEye
+
+open Swensen.Utils
+open System.Windows.Forms
+
+open System
+open System.Reflection
+open ImpromptuInterface.FSharp
+
+let sendToLabel(tn:TreeNode) =
+    let pm = tn.TreeView?pluginManager?Value
+    pm
+
+[<Fact(Skip="not done yet: trying to figure best way create mock plugin")>]
+let ``root watch SendTo label value`` () =
+    let pm = new PluginManager(new TabControl())
+    let plugin = {Plugin=Unchecked.defaultof<IPlugin>;PluginManager=pm}
+    pm?managedPlugins
+
+    let tree = new WatchTreeView(Some(pm))
+    tree.Watch("watch", [1;2;3;4;5])
+    let tn = tree.Nodes.[0]
+
+
+    test <@ sendToLabel tn <> null @>
