ef1b39bce6efc1f3c4d5c8c57960481711c61a59
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-07-09 05:52:28 +0000

    created "findTreeNode" test helper

--- Test.FsEye/PluginManagerTests.fs
+++ Test.FsEye/PluginManagerTests.fs
@@ -52,3 +52,31 @@ let ``root watch SendTo label value`` () =
     let tn = tree.Nodes.[0]
 
     test <@ tree?calcNodeLabel(tn) = "watch" @>
+
+///Parses the path to find a tree node in the Tree, expanding nodes as needed. Path has form:
+///root/child1/child2/child3 where the root and each child is a text starts with match
+let findTreeNode (tree:TreeView) (path:string) =
+    let parts = path.Split('/') |> Seq.toList
+
+    let find (nodes:TreeNodeCollection) p = nodes |> Seq.cast<TreeNode> |> Seq.find (fun x -> x.Text.StartsWith(p))
+
+    let rec loop tn parts =
+        match parts with
+        | [] -> tn
+        | p::parts' when tn = null ->
+            let tn' = find tree.Nodes p
+            loop tn' parts'
+        | p::parts' ->
+            tree?OnAfterExpand(new TreeViewEventArgs(tn))    
+            let tn' = find tn.Nodes p
+            loop tn' parts'
+            
+    loop null parts
+        
+
+[<Fact>]
+let ``non virtual method SendTo label value`` () =
+    let tree = new WatchTreeView()
+    tree.Watch("watch", ResizeArray({1..5}))
+    let tn = findTreeNode tree "watch/ToArray"
+    test <@ tree?calcNodeLabel(tn) = "watch.ToArray()" @>
