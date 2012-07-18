ac9392d68809d41629a1f53f5ea8f8f66116508f
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-07-18 02:49:15 +0000

    changed WatchTreeView.Watch to string * obj * Type since type param for the second args was not being used.

--- trunk/FsEye/Forms/WatchPanel.fs
+++ trunk/FsEye/Forms/WatchPanel.fs
@@ -60,10 +60,10 @@ type WatchPanel() as this =
         //a lot of delegation to treeView below -- not sure how to do this better
 
         ///Add or update a watch with the given name, value, and type.
-        member this.Watch(name, value, ty) =
+        member this.Watch(name, value:obj, ty) =
             splitContainer.TreeView.Watch(name, value, ty)
 
-        ///Add or update a watch with the given name and value.
+        ///Add or update a watch with the given name and value (where the type is derived from the type paramater of the value).
         member this.Watch(name, value) =
             splitContainer.TreeView.Watch(name,value)
 
