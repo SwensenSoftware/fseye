17080acd139362c31a699743f88da6f0abd3e0a4
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-07-20 12:51:14 +0000

    tagging trunk at rev. 379 for 2.0.0-beta1 release which features the first beta preview of the upcoming plugin system.

---
+++ tags/2.0.0-beta1/Debug.FsEye/Program.fs
@@ -0,0 +1,21 @@
+﻿namespace Debug.FsEye
+
+open System
+open System.Drawing
+open System.Windows.Forms
+
+module Main =
+    let initEye() =
+        let eye = new Swensen.FsEye.Forms.EyeForm()   
+        eye.Watch("x", 3)  
+        eye.Watch("y", new System.Collections.Generic.List<int>(Seq.init 200 id))      
+        eye.Watch("some null value", null, typeof<System.Collections.Generic.Dictionary<int,string>>)
+        eye
+
+
+    [<STAThread>]
+    [<EntryPoint>]
+    do
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(false)
+        Application.Run(initEye()) 
---
+++ tags/2.0.0-beta1/Test.FsEye/PluginManagerTests.fs
@@ -0,0 +1,105 @@
+﻿module PluginManagerTests
+
+open Swensen.Unquote
+open Xunit
+
+open Swensen.FsEye.WatchModel
+open Swensen.FsEye.Forms
+open Swensen.FsEye
+
+open Swensen.Utils
+open System.Windows.Forms
+
+open System
+open System.Reflection
+open ImpromptuInterface.FSharp
+
+let mkPlugin name mkControl =
+    {
+        new IPlugin with
+            member __.Name = name
+            member __.IsWatchable(_,_) = true
+            member __.CreateWatchViewer() =
+                { 
+                    new IWatchViewer with
+                        member __.Watch(_,_,_) = ()
+                        member __.Control = mkControl()
+                }
+    }
+
+let mkControl = fun () -> new Control()
+
+[<Fact>]
+let ``SendTo plugin creates watch viewers with absolute count ids`` () =
+    let plugin = mkPlugin "Plugin" mkControl
+    let pm = new PluginManager()
+    let mp = pm.RegisterPlugin(plugin)
+    test <@ pm.SendTo(mp, "", null, typeof<obj>).ID = "Plugin 1" @>
+    test <@ pm.SendTo(mp, "", null, typeof<obj>).ID = "Plugin 2" @>
+    test <@ pm.SendTo(mp, "", null, typeof<obj>).ID = "Plugin 3" @>
+    pm.RemoveManagedWatchViewer("Plugin 3")
+    test <@ pm.SendTo(mp, "", null, typeof<obj>).ID = "Plugin 4" @>
+
+[<Fact>]
+let ``removing a plugin removes its watch viewers`` () =
+    let pluginA = mkPlugin "PluginA" mkControl
+    let pluginB = mkPlugin "PluginB" mkControl
+
+    let pm = new PluginManager()
+    let mpA = pm.RegisterPlugin(pluginA)
+    let mpB = pm.RegisterPlugin(pluginB)
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpB, "", null, typeof<obj>) |> ignore
+
+    pm.RemoveManagedPlugin(mpB)
+
+    test <@ pm.ManagedWatchViewers |> Seq.map (fun x -> x.ID) |> Seq.toList = ["PluginA 1"; "PluginA 2"]  @>
+
+[<Fact>]
+let ``registering a plugin of the same name removes the previous version of the plugin`` () =
+    let pluginA = mkPlugin "PluginA" mkControl
+    let pluginB = mkPlugin "PluginB" mkControl
+
+    let pm = new PluginManager()
+    let mpA = pm.RegisterPlugin(pluginA)
+    let mpB = pm.RegisterPlugin(pluginB)
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpB, "", null, typeof<obj>) |> ignore
+
+    let pluginB' = mkPlugin "PluginB" mkControl
+    let mpB' = pm.RegisterPlugin(pluginB')
+
+    ///i.e. all of the original PluginB watch viewers were removed when the new version of registered
+    test <@ pm.ManagedWatchViewers |> Seq.map (fun x -> x.ID) |> Seq.toList = ["PluginA 1"; "PluginA 2"]  @>
+
+    test <@ pm.ManagedPlugins |> Seq.map (fun x -> x.Plugin) |> Seq.toList = [pluginA; pluginB']  @>
+
+[<Fact>]
+let ``removing a managed watch viewer disposes of its control`` () =
+    let control = new Control()
+    let pluginA = mkPlugin "PluginA" (fun () -> control)
+
+    let pm = new PluginManager()
+    let mpA = pm.RegisterPlugin(pluginA)
+    let mwv = pm.SendTo(mpA, "", null, typeof<obj>)
+    
+    test <@ not mwv.WatchViewer.Control.IsDisposed @>
+    pm.RemoveManagedWatchViewer(mwv)
+    test <@ mwv.WatchViewer.Control.IsDisposed @>
+
+[<Fact>]
+let ``ManagedPlugin ManagedWatchViewers filtered correctly`` () =
+    let pluginA = mkPlugin "PluginA" mkControl
+    let pluginB = mkPlugin "PluginB" mkControl
+
+    let pm = new PluginManager()
+    let mpA = pm.RegisterPlugin(pluginA)
+    let mpB = pm.RegisterPlugin(pluginB)
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpB, "", null, typeof<obj>) |> ignore
+
+    test <@ mpA.ManagedWatchViewers |> Seq.map (fun x -> x.ID) |> Seq.toList = ["PluginA 1"; "PluginA 2"]  @>
+    test <@ mpB.ManagedWatchViewers |> Seq.map (fun x -> x.ID) |> Seq.toList = ["PluginB 1";]  @>
