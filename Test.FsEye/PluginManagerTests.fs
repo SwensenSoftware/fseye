7c41a4bb1d984da1677891ca2b2e2a87228fb99a
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-07-20 00:46:41 +0000

    more tests

--- Test.FsEye/PluginManagerTests.fs
+++ Test.FsEye/PluginManagerTests.fs
@@ -14,109 +14,6 @@ open System
 open System.Reflection
 open ImpromptuInterface.FSharp
 
-///Parses the path to find a tree node in the Tree, expanding nodes as needed. Path has form:
-///root/child1/child2/child3 where t%he root and each child is a text starts with match
-let findTreeNode (tree:TreeView) (path:string) =
-    let parts = path.Split('/') |> Seq.toList
-
-    let tryFind (nodes:TreeNodeCollection) p = nodes |> Seq.cast<TreeNode> |> Seq.tryFind (fun x -> x.Text.StartsWith(p))
-
-    let rec loop tn parts =
-        match parts with
-        | [] -> tn
-        | p::parts' when tn = null -> //the root
-            match tryFind tree.Nodes p with
-            | Some(tn') -> loop tn' parts'
-            | None -> failwith "unexpected condition"
-        | p::parts' ->
-            //expand tn nodes in another thread so thread sleeping on this thread doesn't block loading work
-            async {
-                do! Async.SwitchToNewThread()
-                tree?OnAfterExpand(new TreeViewEventArgs(tn))
-            } |> Async.RunSynchronously
-
-            let rec waitLoop = function
-                | None ->
-                    System.Threading.Thread.Sleep(120)
-                    waitLoop (tryFind tn.Nodes p)
-                | Some(tn') -> tn'
-                    
-            let tn' = waitLoop None
-            loop tn' parts'
-            
-    loop null parts
-
-[<Fact>]
-let ``watch label: root watch`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", [1;2;3;4;5])
-    let tn = tree.Nodes.[0]
-    test <@ tree?calcNodeLabel(tn) = "watch" @>
-
-[<Fact>]
-let ``watch label: non virtual method`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    let tn = findTreeNode tree "watch/ToArray()"
-    test <@ tree?calcNodeLabel(tn) = "watch.ToArray()" @>
-
-[<Fact>]
-let ``watch label: base class virtual method`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    let tn = findTreeNode tree "watch/obj.ToString()"
-    test <@ tree?calcNodeLabel(tn) = "watch.ToString()" @>
-
-[<Fact>]
-let ``watch label: non virtual property`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    let tn = findTreeNode tree "watch/Count"
-    test <@ tree?calcNodeLabel(tn) = "watch.Count" @>
-
-[<Fact>]
-let ``watch label: interface virtual property`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    let tn = findTreeNode tree "watch/IList.IsFixedSize"
-    test <@ tree?calcNodeLabel(tn) = "(watch :> IList).IsFixedSize" @>
-
-[<Fact>]
-let ``watch label: non virtual method of interface virtual property`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    let tn = findTreeNode tree "watch/IList.IsFixedSize/GetHashCode()"
-    test <@ tree?calcNodeLabel(tn) = "(watch :> IList).IsFixedSize.GetHashCode()" @>
-
-[<Fact>]
-let ``watch label: enumerator element`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    let tn = findTreeNode tree "watch/Non-public/obj.MemberwiseClone()"
-    test <@ tree?calcNodeLabel(tn) = "watch?MemberwiseClone()" @>
-
-[<Fact>]
-let ``watch label: enumerator rest element`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..110}))
-    let tn = findTreeNode tree "watch/GetEnumerator()/Rest/[103]"
-    test <@ tree?calcNodeLabel(tn) = "watch.GetEnumerator().[103]" @>
-
-[<Fact>]
-let ``watch label: private method`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..110}))
-    let tn = findTreeNode tree "watch/GetEnumerator()/Rest/[103]"
-    test <@ tree?calcNodeLabel(tn) = "watch.GetEnumerator().[103]" @>
-
-[<Fact>]
-let ``watch label: archive`` () =
-    let tree = new WatchTreeView()
-    tree.Watch("watch", ResizeArray({1..5}))
-    tree.Archive()
-    let tn = findTreeNode tree "Archive (0)/watch"
-    test <@ tree?calcNodeLabel(tn) = "[Archive (0)] watch" @>
-
 let mkPlugin name mkControl =
     {
         new IPlugin with
@@ -192,3 +89,17 @@ let ``removing a managed watch viewer disposes of its control`` () =
     pm.RemoveManagedWatchViewer(mwv)
     test <@ mwv.WatchViewer.Control.IsDisposed @>
 
+[<Fact>]
+let ``ManagedPlugin ManagedWatchViewers filtered correctly`` () =
+    let pluginA = mkPlugin "PluginA" mkControl
+    let pluginB = mkPlugin "PluginB" mkControl
+
+    let pm = new PluginManager()
+    let mpA = pm.RegisterPlugin(pluginA)
+    let mpB = pm.RegisterPlugin(pluginB)
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpA, "", null, typeof<obj>) |> ignore
+    pm.SendTo(mpB, "", null, typeof<obj>) |> ignore
+
+    test <@ mpA.ManagedWatchViewers |> Seq.map (fun x -> x.ID) |> Seq.toList = ["PluginA 1"; "PluginA 2"]  @>
+    test <@ mpB.ManagedWatchViewers |> Seq.map (fun x -> x.ID) |> Seq.toList = ["PluginB 1";]  @>
