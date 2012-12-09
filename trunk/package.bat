0fff41474099818e8fb24491fa3b154e96f20a52
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2012-12-09 17:02:00 +0000

    improve package.bat

--- trunk/package.bat
+++ trunk/package.bat
@@ -1,18 +1,34 @@
 set /p versionNumber=
 
+REM clean up
+del builds\FsEye-%versionNumber%.zip
 rd /q /s builds\FsEye-%versionNumber%
+rd /q /s builds\FsEye
 
-mkdir builds\FsEye-%versionNumber%
-mkdir builds\FsEye-%versionNumber%\plugins
+REM preparing staging dir
+mkdir staging
+mkdir staging\plugins
 
-copy LICENSE builds\FsEye-%versionNumber%
-copy NOTICE builds\FsEye-%versionNumber%
+copy LICENSE staging
+copy NOTICE staging
 
-copy FsEye\bin\Release\FsEye.fsx builds\FsEye-%versionNumber%
-copy FsEye\bin\Release\FsEye.dll builds\FsEye-%versionNumber%
-copy FsEye\bin\Release\FsEye.xml builds\FsEye-%versionNumber%
+copy FsEye\bin\Release\FsEye.fsx staging
+copy FsEye\bin\Release\FsEye.dll staging
+copy FsEye\bin\Release\FsEye.xml staging
 
-copy "FsEye.PropertyGrid.Plugin\bin\Release\FsEye.PropertyGrid.Plugin.dll" builds\FsEye-%versionNumber%\plugins
-copy "FsEye.TreeView.Plugin\bin\Release\FsEye.TreeView.Plugin.dll" builds\FsEye-%versionNumber%\plugins
+copy "FsEye.PropertyGrid.Plugin\bin\Release\FsEye.PropertyGrid.Plugin.dll" staging\plugins
+copy "FsEye.TreeView.Plugin\bin\Release\FsEye.TreeView.Plugin.dll" staging\plugins
+
+REM zip staging files
+cd staging
+"..\tools\7z\7za.exe" a -tzip "..\builds\FsEye-%versionNumber%.zip" *
+cd ..
+
+REM extract build
+"tools\7z\7za.exe" x "builds\FsEye-%versionNumber%.zip" -o"builds\FsEye-%versionNumber%"
+"tools\7z\7za.exe" x "builds\FsEye-%versionNumber%.zip" -o"builds\FsEye"
+
+REM clean up
+rd /q /s staging
 
 pause
