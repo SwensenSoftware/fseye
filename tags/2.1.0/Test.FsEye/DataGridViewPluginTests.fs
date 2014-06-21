4b901d8632e5521c6eac229d367706f3f33057ec
stephen.swensen <stephen.swensen@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2014-06-21 10:53:14 +0000

    tagging the 2.1.0 release from trunk

---
+++ tags/2.1.0/Test.FsEye/DataGridViewPluginTests.fs
@@ -0,0 +1,53 @@
+﻿(*
+Copyright 2011 Stephen Swensen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*)
+module DataGridViewTests
+
+open Swensen.Unquote
+open Xunit
+
+open Swensen.FsEye
+open Swensen.FsEye.Plugins
+
+open Swensen.Utils
+open System.Windows.Forms
+
+[<Fact>]
+let ``seq is synthetic watchable type`` () =
+    let p = new DataGridViewPlugin() :> IPlugin
+    let xs = seq [1;2;3]
+    test <@ p.IsWatchable(xs, xs.GetType()) @>
+
+    let wv = p.CreateWatchViewer()
+    wv.Watch("", xs, xs.GetType())
+    let dgv = wv.Control.Controls |> Seq.cast<Control> |> Seq.find (fun c -> c :? DataGridView) :?> DataGridView
+    let xsWrapper = dgv.DataSource
+    test <@ xsWrapper :? System.Collections.Generic.List<obj> @>
+    let xsWrapper = xsWrapper :?> System.Collections.Generic.List<obj>
+    test <@ xsWrapper.Count = 3 @>
+    test <@ xsWrapper.[0] :?> int = 1 @>
+    test <@ xsWrapper.[1] :?> int = 2 @>
+    test <@ xsWrapper.[2] :?> int = 3 @>
+
+[<Fact>]
+let ``ResizeArray is a natively watchable type`` () =
+    let p = new DataGridViewPlugin() :> IPlugin
+    let xl = new System.Collections.Generic.List<int>([1;2;3])
+    test <@ p.IsWatchable(xl, xl.GetType()) @>
+
+    let wv = p.CreateWatchViewer()
+    wv.Watch("", xl, xl.GetType())
+    let dgv = wv.Control.Controls |> Seq.cast<Control> |> Seq.find (fun c -> c :? DataGridView) :?> DataGridView
+    test <@ obj.ReferenceEquals(xl, dgv.DataSource) @>
---
+++ tags/2.1.0/package.bat
@@ -0,0 +1,59 @@
+set /p versionNumber=
+
+REM clean up
+del builds\FsEye-%versionNumber%.zip
+del builds\FsEye.%versionNumber%.nupkg
+rd /q /s builds\FsEye-%versionNumber%
+rd /q /s builds\FsEye
+
+REM preparing staging dir
+mkdir staging
+mkdir staging\plugins
+
+copy LICENSE staging
+copy NOTICE staging
+
+copy FsEye\bin\Release\FsEye.fsx staging
+copy FsEye\bin\Release\FsEye.dll staging
+copy FsEye\bin\Release\FsEye.xml staging
+
+copy "FsEye.PropertyGrid.Plugin\bin\Release\FsEye.PropertyGrid.Plugin.dll" staging\plugins
+copy "FsEye.TreeView.Plugin\bin\Release\FsEye.TreeView.Plugin.dll" staging\plugins
+copy "FsEye.DataGridView.Plugin\bin\Release\FsEye.DataGridView.Plugin.dll" staging\plugins
+
+REM zip staging files
+cd staging
+"..\tools\7z\7za.exe" a -tzip "..\builds\FsEye-%versionNumber%.zip" *
+cd ..
+
+REM extract build
+"tools\7z\7za.exe" x "builds\FsEye-%versionNumber%.zip" -o"builds\FsEye-%versionNumber%"
+"tools\7z\7za.exe" x "builds\FsEye-%versionNumber%.zip" -o"builds\FsEye"
+
+REM preparing nuget dirs
+
+mkdir nuget
+mkdir nuget\content
+mkdir nuget\lib
+mkdir nuget\lib\net40
+mkdir nuget\lib\net40\plugins
+copy FsEye.nuspec nuget
+
+REM copy staging builds to nuget package...
+
+copy staging\FsEye.dll nuget\lib\net40\FsEye.dll
+copy staging\plugins\* nuget\lib\net40\plugins\
+copy FsEye\bin\Release\FsEye.NuGet.fsx nuget\content\FsEye.fsx
+
+REM create nuget package...
+
+".nuget\nuget.exe" pack nuget\FsEye.nuspec -Version %versionNumber%
+copy FsEye.%versionNumber%.nupkg builds
+del FsEye.%versionNumber%.nupkg
+
+REM cleanup...
+
+rd /q /s staging
+rd /q /s nuget
+
+pause
