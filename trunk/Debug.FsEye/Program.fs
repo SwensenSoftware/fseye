2114f84599cf54be4987253bf64583ca79706d3a
stephen.swensen@gmail.com <stephen.swensen@gmail.com@bb255904-8bbc-056b-5ecb-5ddaece8e250>
2014-06-27 03:07:57 +0000

    Issue 44: fixed. we swallow dataerror events that seem to arise from non-homoeneous collections

--- trunk/Debug.FsEye/Program.fs
+++ trunk/Debug.FsEye/Program.fs
@@ -3,8 +3,20 @@
 open System
 open System.Drawing
 open System.Windows.Forms
+open System.Xml
 
-type Student = { mutable Name:string; Age:int }
+type IHuman =
+    abstract member Name : string
+
+type Student = 
+    { Name:string; Age:int }
+        interface IHuman with
+            member this.Name = this.Name
+
+type Professor = 
+    { Name:string; Age:int; Rank: int }
+        interface IHuman with
+            member this.Name = this.Name
 
 module Main =
     let initEye() =
@@ -14,8 +26,21 @@ module Main =
         eye.Watch("some null value", null, typeof<System.Collections.Generic.Dictionary<int,string>>)
         let value = ([|3.2; 2.; 1.; -3.; 23.|],[|"a";"b";"c";"d";"e"|])
         eye.Watch("series", value, value.GetType())
-        let value = [{Name="Tom"; Age=3};{Name="Jane"; Age=9}] 
+        let value = [{Student.Name="Tom"; Age=3};{Name="Jane"; Age=9}] 
         eye.Watch("series2", value, value.GetType())
+
+        let value = [
+            {Professor.Name="Jane"; Age=9; Rank=23} :> IHuman; 
+            {Student.Name="Tom"; Age=3} :> IHuman; ]
+        eye.Watch("series3", value, value.GetType())
+
+        let doc = new XmlDocument()
+        doc.AppendChild(doc.CreateProcessingInstruction("xml", "version=\"1.0\" encoding=\"utf-8\"")) |> ignore
+        let root = doc.CreateElement("Root")
+        root.AppendChild(doc.CreateElement("Apple"))|>ignore
+        doc.AppendChild(root)|>ignore
+        eye.Watch("xmldoc", doc)
+
         eye.Watch("eye", eye, eye.GetType())
         eye
 
