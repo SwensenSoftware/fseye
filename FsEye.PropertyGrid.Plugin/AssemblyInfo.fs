af9dab5e2a528982a9de057de0a5a4394f1ef8d9
Stephen Swensen <stephen.swensen@gmail.com>
2016-04-16 01:50:30 -0400

    update version numbers so fourth digit is explicit - odd implies alpha release. old scheme was incorrect

--- FsEye.PropertyGrid.Plugin/AssemblyInfo.fs
+++ FsEye.PropertyGrid.Plugin/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("FsEye.PropertyGrid.Plugin")>]
-[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2012")>]
+[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2016")>]
 [<assembly: AssemblyTrademark("")>]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -30,10 +30,7 @@ open System.Runtime.InteropServices
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("1.0.0.*")>]
-[<assembly: AssemblyFileVersion("1.0.0.*")>]
+//the fourth position is for beta release numbers
+[<assembly: AssemblyVersion("1.0.0.1")>]
 
 ()
--- FsEye/AssemblyInfo.fs
+++ FsEye/AssemblyInfo.fs
@@ -27,7 +27,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("FsEye")>]
-[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2014")>]
+[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2016")>]
 [<assembly: AssemblyTrademark("")>]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -45,10 +45,7 @@ open System.Runtime.InteropServices
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("2.1.0.*")>] //the fourth position is for beta release numbers
-[<assembly: AssemblyFileVersion("2.1.0.*")>]
+//the fourth position is for beta release numbers
+[<assembly: AssemblyVersion("2.1.1.1")>]
 
 ()
